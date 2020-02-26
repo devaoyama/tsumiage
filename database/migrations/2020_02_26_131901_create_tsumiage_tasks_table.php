c5cf3f63dcd06e6bf8ce3c61a0424d9ce7092927
keita <aoyamakeita@aoyamakeidainoMacBook-Air.local>
2020-02-26 22:20:39 +0900

    マイグレーションの追加

--- database/migrations/2020_02_26_131901_create_tsumiage_tasks_table.php
+++ database/migrations/2020_02_26_131901_create_tsumiage_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTsumiageTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tsumiage_tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('nickname');
-            $table->string('twitter_id');
-            $table->string('twitter_token');
-            $table->string('twitter_token_secret');
-            $table->rememberToken();
+            $table->bigInteger('tsumiage_date_id')->unsigned();
+            $table->string('title');
+            $table->boolean('status')->default(false);
             $table->timestamps();
+
+            $table->foreign('tsumiage_date_id')->references('id')->on('tsumiage_dates');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tsumiage_tasks');
     }
 }
